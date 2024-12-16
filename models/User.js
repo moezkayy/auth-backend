e63a2a1359c5ed2489c7f4064da247610137ea23
moezkayy <moezkayy@gmail.com>
2024-12-16 21:26:31 +0500

    user.js

--- models/User.js
+++ models/User.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');  // Add this line to import mongoose
+
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
